9201f8d36a020b1fdb81d3811437afa68548ee45
KozyrevychYaroslav <kozyrevich.yaroslav@gmail.com>
2021-03-11 22:46:45 +0300

    corrected TestBinarySearch

--- src/ai181/kozyrevych/TestBinarySearch.java
+++ src/ai181/kozyrevych/TestBinarySearch.java
@@ -6,20 +6,15 @@ import org.junit.jupiter.api.RepeatedTest;
 import java.util.Arrays;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestBinarySearch {
-    @DisplayName("Testing Quick sort")
+
+    @DisplayName("Binary search")
     @RepeatedTest(value = 100)
-    public void testSort() {
-        int[] nums = new Random().ints(10000, 0, 10000).toArray();
-        int[] nums_copy = Arrays.copyOf(nums, nums.length);
-
-        Arrays.parallelSort(nums_copy);
-        assertFalse(Arrays.equals(nums_copy, nums));
-
+    public void bs() {
+        int[] nums = new Random().ints(1000, 0, 1000).toArray();
         QuickSort.sort(nums);
-        assertTrue(Arrays.equals(nums_copy, nums));
+        assertEquals(Arrays.binarySearch(nums, 100), BinarySearch.binarySearch(nums, 100));
     }
 }
